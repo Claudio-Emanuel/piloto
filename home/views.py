34ffaff9ef395d5ed50a7e352f40a2b5eeb041f7
Claudio-Emanuel <cemanuelcampos13@gmail.com>
2024-11-12 03:12:55 -0300

    Deu certo V1.4

--- home/views.py
+++ home/views.py
@@ -8,10 +8,10 @@ def index(request):
     return render(request,'index.html')
 
 def sobre(request):
-    return HttpResponse("<h1>Sistema 1.0 desenvolvido por mim</h1>")
+    return render(request,'sobre.html')
 
 def contato(request):
-    return HttpResponse("<h1>Esta eh a pagina de contato</h1>")
+    return render(request,'contato.html')
 
 def ajuda(request):
     return HttpResponse("<h1>Esta eh a pagina de ajuda</h1>")
