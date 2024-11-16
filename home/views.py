5f77c7be8db7fabf9f6bae9bd9131e2a130c52e4
Claudio-Emanuel <cemanuelcampos13@gmail.com>
2024-11-15 23:54:15 -0300

    Deu certo V1.5

--- home/views.py
+++ home/views.py
@@ -13,5 +13,29 @@ def sobre(request):
 def contato(request):
     return render(request,'contato.html')
 
-def ajuda(request):
+def ajuda():
     return HttpResponse("<h1>Esta eh a pagina de ajuda</h1>")
+
+def exibir_item(request, id):
+    return render(request, 'exibir_item.html',{'id':id})
+
+def perfil(request, usuario):
+    return render(request, 'perfil.html',{'str':usuario})
+
+def semana(request, num):
+    if num==1 :
+        return render(request, 'semana.html',{"mensagem":"Domingo"})
+    if num==2 :
+        return render(request, 'semana.html',{"mensagem":"Segunda-Feira"})
+    if num==3 :
+        return render(request, 'semana.html',{"mensagem":"Terça-Feira"})
+    if num==4 :
+        return render(request, 'semana.html',{"mensagem":"Quarta-Feira"})
+    if num==5 :
+        return render(request, 'semana.html',{"mensagem":"Quinta-Feira"})
+    if num==6 :
+        return render(request, 'semana.html',{"mensagem":"Sexta-Feira"})
+    if num==7 :
+        return render(request, 'semana.html',{"mensagem":"Sabado-Feira"})
+    else:
+        return render(request, 'semana.html',{"mensagem":"O numero passado corresponde a um num invalido"})
