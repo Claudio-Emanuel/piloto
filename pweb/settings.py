aebad3793c64898e5696ce4b756b35a2d3db74d1
Claudio-Emanuel <cemanuelcampos13@gmail.com>
2024-11-24 01:04:18 -0300

    Deu certo V1.7.1

--- pweb/settings.py
+++ pweb/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)3v3q(i(=$=58g0jk%l_9@s&_5%dur)bi=9*v(j97z93j25jtt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','.vercel.app']
 
